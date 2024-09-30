a093917ed6d2ac9805df1dec39dcd2449ca02b45
Chris.Jochem <chris.jochem@os.uk>
2024-09-30 11:17:54 +0100

    feat: make BNG validation pattern

---
+++ data-raw/make_internal_data.R
@@ -0,0 +1,21 @@
+# code to prepare internal datasets
+
+# Test for valid BNG patterns
+# constrained to GB-specific bounding area
+# spaces allowed, all are optional
+# limited to >= 1m resolution
+PATTERN <- paste0("^(H[LMNOPQRSTUVWXYZ]|",
+                  "N[ABCDEFGHJKLMNOPQRSTUVWXYZ]|",
+                  "O[ABFGLMQRVW]|",
+                  "S[ABCDEFGHJKLMNOPQRSTUVWXYZ]|",
+                  "T[ABFGLMQRVW]|",
+                  "J[LMQRVW])\\s?",
+                  "(?:(\\d{2}|\\d{4}|\\d{6}|\\d{8}|",
+                  "\\d{1}\\s\\d{1}|\\d{2}\\s\\d{2}|",
+                  "\\d{3}\\s\\d{3}|\\d{4}\\s\\d{4}|)?",
+                  "\\s?(NE|SE|SW|NW)?$|",
+                  "(\\d{10}|\\d{5}\\s\\d{5}$))")
+
+# Store .rda file for the package.
+usethis::use_data(PATTERN,
+                  internal = TRUE, overwrite = TRUE)
