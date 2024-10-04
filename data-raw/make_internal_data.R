3a7162ec3443bc88649248362f1fb235813edc69
Chris.Jochem <chris.jochem@os.uk>
2024-10-04 16:28:55 +0100

    feat: adjust validation regex

--- data-raw/make_internal_data.R
+++ data-raw/make_internal_data.R
@@ -12,9 +12,10 @@ bng_pattern <- paste0("^(H[LMNOPQRSTUVWXYZ]|",
                       "J[LMQRVW])\\s?",
                       "(?:(\\d{2}|\\d{4}|\\d{6}|\\d{8}|",
                       "\\d{1}\\s\\d{1}|\\d{2}\\s\\d{2}|",
-                      "\\d{3}\\s\\d{3}|\\d{4}\\s\\d{4}|)?",
-                      "\\s?(NE|SE|SW|NW)?$|",
-                      "(\\d{10}$|\\d{5}\\s\\d{5}$))")
+                      "\\d{3}\\s\\d{3}|\\d{4}\\s\\d{4}|",
+                      "\\d{10}$|\\d{5}\\s\\d{5}$))",
+                      "\\s?(NE|SE|SW|NW)?$")
+
 
 # Store .rda file for the package.
 usethis::use_data(bng_pattern,
