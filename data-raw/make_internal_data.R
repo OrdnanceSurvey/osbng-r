7515cb27d233ec1b48c3293f49dc41db0ed773f7
Chris.Jochem <chris.jochem@os.uk>
2025-01-16 13:14:58 +0000

    fix: adjust validation pattern

--- data-raw/make_internal_data.R
+++ data-raw/make_internal_data.R
@@ -4,17 +4,17 @@
 # constrained to GB-specific bounding area
 # spaces allowed, all are optional
 # limited to >= 1m resolution
-bng_pattern <- paste0("^(H[LMNOPQRSTUVWXYZ]|",
+bng_pattern <- paste0("(H[LMNOPQRSTUVWXYZ]|",
                       "N[ABCDEFGHJKLMNOPQRSTUVWXYZ]|",
                       "O[ABFGLMQRVW]|",
                       "S[ABCDEFGHJKLMNOPQRSTUVWXYZ]|",
                       "T[ABFGLMQRVW]|",
                       "J[LMQRVW])\\s?",
-                      "(?:(\\d{2}|\\d{4}|\\d{6}|\\d{8}|",
+                      "(\\d{2}|\\d{4}|\\d{6}|\\d{8}|",
                       "\\d{1}\\s\\d{1}|\\d{2}\\s\\d{2}|",
                       "\\d{3}\\s\\d{3}|\\d{4}\\s\\d{4}|",
-                      "\\d{10}$|\\d{5}\\s\\d{5}$))",
-                      "\\s?(NE|SE|SW|NW)?$")
+                      "\\d{10}$|\\d{5}\\s\\d{5}$)?\\s?",
+                      "(NE|SE|SW|NW)?$")
 
 
 # Set of valid BNG prefixes. Arrange in a 7x13 array for geographic positioning
