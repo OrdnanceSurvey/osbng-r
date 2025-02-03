5888085edba7788e1f2f14de58b19395b8bffe20
Chris.Jochem <chris.jochem@os.uk>
2025-02-03 18:48:30 +0000

    test: for valid resolutions

--- tests/testthat/test-valid.R
+++ tests/testthat/test-valid.R
@@ -7,6 +7,17 @@ test_that("invalid BNG references are detected", {
 })
 
 
+test_that("invalid resolutions are detected", {
+  expect_equal(is_valid_bng_resolution(0), FALSE)
+  expect_equal(is_valid_bng_resolution(10), TRUE)
+  expect_equal(is_valid_bng_resolution("100"), FALSE)
+  expect_equal(is_valid_bng_resolution("100m"), TRUE)
+  expect_equal(is_valid_bng_resolution("5km"), TRUE)
+  expect_equal(is_valid_bng_resolution(5000), TRUE)
+  expect_equal(is_valid_bng_resolution(100000), TRUE)
+})
+
+
 test_that("resolutions are returned as integers", {
   df <- readRDS(test_cases("get_bng_resolution"))
   for (i in 1:nrow(df)) {
