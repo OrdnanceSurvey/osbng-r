061374cae1f87e3353fe76ae6a893583bc2e307f
Chris.Jochem <chris.jochem@os.uk>
2025-02-05 16:20:29 +0000

    feat: identify contiguous neighbours

--- R/traversal.R
+++ R/traversal.R
@@ -1,5 +1,5 @@
 
-#' Traverse BNG references
+#' Spatial neighbourhoods
 #' 
 #' Identify neighbours in a hollow ring or solid disc at grid distance 'k' from
 #' a target BNG reference.
@@ -41,7 +41,7 @@ bng_kring <- function(bng_ref, k) {
     ref <- bng_ref[i]
     resolution <- internal_get_resolution(ref)
     
-    refs <- get_disc_neighbours(ref, resolution, k, TRUE)
+    refs <- get_disc_neighbours(ref, resolution, k, type = "ring")
     
     return(refs)
   })
@@ -81,7 +81,7 @@ bng_kdisc <- function(bng_ref, k) {
     ref <- bng_ref[idx]
     resolution <- internal_get_resolution(ref)
     
-    neighs <- get_disc_neighbours(ref, resolution, k, FALSE)
+    neighs <- get_disc_neighbours(ref, resolution, k, type = "disc")
     return(neighs)
   })
   
@@ -92,27 +92,73 @@ bng_kdisc <- function(bng_ref, k) {
 }
 
 
+#' Identify neighbours
+#' 
+#' Find BNG references which share a grid cell edge with a target BNG reference.
+#' @param bng_ref,bng_ref1 target object of type \code{BNGReference}
+#' @param bng_ref2 \code{BNGReference} object for comparison when assessing
+#'   neighbour relationships
+#' @details Grid reference cells are "neighbours" when they share a contiguous
+#' edge (i.e. corners do not define neighbours). In the event that a target
+#' reference is along the edge or corner of the valid BNG area, then 3 or 2
+#' references, respectively, will be returned \code{bng_is_neighbour} only
+#' compares references of equal resolution.
+#' @returns A set of up to four \code{BNGReference} objects that border the
+#'   target reference for \code{bng_neighbours}, while \code{bng_neighbours}
+#'   returns a boolean.
+#' 
+#' @name bng_neighbours
+#' @export
+bng_neighbours <- function(bng_ref) {
+  validate_bng_ref(bng_ref)
+  
+  # set up return
+  neigh_list <- vector("list", length = length(bng_ref))
+  valid_idx <- !is.na(bng_ref)
+
+  neighbours <- lapply(seq_along(bng_ref), function(idx) {
+    ref <- bng_ref[idx]
+    resolution <- internal_get_resolution(ref)
+    
+    neighs <- get_disc_neighbours(ref, resolution, k = 1, type = "rook")
+    return(neighs)
+  })
+  
+  # replace valid
+  neigh_list[valid_idx] <- neighbours
+  
+  neigh_list
+}
+
+
+#' @aliases bng_neighbours
+#' @export
 bng_is_neighbour <- function(bng_ref1, bng_ref2) {
   
 }
 
 
-bng_neighbours <- function(bng_ref) {
-  
-}
-
 # Helper function to handle internal neighbour generation
 #' @keywords internal
 #' @noRd
-get_disc_neighbours <- function(ref, resolution, k, ring = FALSE) {
+get_disc_neighbours <- function(ref, 
+                                resolution, 
+                                k, 
+                                type = c("disc", "ring", "rook")) {
+  type = match.arg(type)
+  
   # calculate relative neighbours
   i <- c(-k:k)
   g <- expand.grid("x" = i, "y" = i)
   g <- g[!(g$x == 0 & g$y == 0), ]
   
   # subset to a ring
-  if (ring == TRUE) {
+  if (type == "ring") {
     g <- g[g$x %in% c(-k, k) | g$y %in% c(-k, k), ]
+  }
+  
+  if (type == "rook") {
+    g <- g[g$x == 0 | g$y == 0, ]
   }
   
   # get starting point
