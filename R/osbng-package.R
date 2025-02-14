ef2267858801cc76634d090652b3586e24339873
Chris.Jochem <chris.jochem@os.uk>
2025-02-14 16:36:54 +0000

    docs: update package description

--- R/osbng-package.R
+++ R/osbng-package.R
@@ -1,3 +1,13 @@
+#' \code{osbng}: Access the British National Grid (BNG) index system
+#' 
+#' \code{osbng} provides a programmatic interface to Ordnance Survey's British
+#' National Grid (BNG) index system, supporting efficient grid-based indexing
+#' and spatial analysis. This enables applications such as statistical
+#' aggregation, data visualisation, and data interoperability using BNG grid
+#' references. Designed for developers working with geospatial data in the
+#' context of Great Britain, the library offers tools to simplify working with
+#' the BNG, facilitating both technical integration into geospatial workflows
+#' and practical exploration of the index system's structure.
 #' @keywords internal
 "_PACKAGE"
 
